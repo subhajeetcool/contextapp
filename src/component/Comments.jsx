cc6fb13bb7078b483fbd2f1b047df966996d5faa
subhajeetcool <subhajeet37@gmail.com>
2023-06-14 12:54:37 +0530

    complete updated context

--- src/component/Comments.jsx
+++ src/component/Comments.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../Context'
 
 function Comments() {
+    const context = useContext(DataContext)
+    const [comments] = context.commentApi.comments
+
+
+
   return (
-    <div>
-      
+    <div className="container">
+      <div className="row">
+        <div className='col-md-12 text-center'>
+          <h3 className='display-3 text-success'>Comments</h3>
+
+        </div>
+      </div>
+      <div className="row">
+        {
+          comments && comments.map((item,index) => {
+            return (
+              <div className='col-md-4 mt-2 mb-2' key={index}>
+                <div className="card">
+                  <div className="card-header">
+                    <h6 className='text-center'>{item.id} {item.name}</h6>
+                    </div>
+                    <div className="card-body">
+                      <p className="text-secondary">
+                        {item.body}
+                      </p>
+                      </div>
+                      <div className="card footer">
+                        <p className='text-success float-end'>
+                          {item.body}
+                        </p>
+
+                      </div>
+                    
+                  
+                </div>
+
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../Context'
 
 function Posts() {
+
+  const context = useContext(DataContext)
+  const [posts] = context.postApi.posts
   return (
-    <div>
-      
+
+    <div className="container">
+      <div className="row">
+        <div className='col-md-12 text-center'>
+          <h3 className='display-3 text-success'>Posts</h3>
+        </div>
+      </div>
+
+      <div className="row">
+        {
+          posts && posts.map((item, index) => {
+            return (
+              <div className='col-md-4 mt-2 mb-2' key={index}>
+                <div className="card">
+                  <div className="card-header">
+                    <h6 className='text-center'>{item.id} {item.title}</h6>
+                  </div>
+                  <div className="card-body">
+                    <p className="text-secondary">
+                      {item.body}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
